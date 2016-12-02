f926878d674a04d22aca2f1c991177a261d6dd62
Jonathan Reyes <jonathan.js.reyes@gmail.com>
2016-12-01 20:30:44 -0500

    repackaged for Chrome and Opera, updated manifest to v0.2

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # rSlash
-Opera/Chrome omnibox extension to quick-nav to a particular subreddit, using "r/" as the keyword
+Opera/Chrome omnibox extension to quick-nav to a particular subreddit, using "r" as the keyword
 
 To Use:
-Type "r/ \<sub\>" into the omnibox and hit enter to navigate to reddit.com/r/\<sub\>
-NOTE: you can also just type "r <sub>"; the forward slash is optional
+Type "r \<sub\>" into the omnibox and hit enter to navigate to reddit.com/r/\<sub\>
 
 To-Do's:
--Implement suggestsions based on list of all subreddits
-	-Filter based on popularity, NSFW, etc. from extension menu 
+  -Implement suggestsions based on list of all subreddits
+  -Filter based on popularity, NSFW, etc. from extension menu 
--- Source/background.js
+++ Source/background.js
@@ -1,23 +1,27 @@
 //Listener for whenever the user changes the text in the omnibox.
 //@TODO: implement subreddit suggestions
 chrome.omnibox.onInputChanged.addListener (
-	function(text, suggest) {
-		return;
-	}
+  function(text, suggest) {
+    return;
+  }
 );
 
-//Listener for whenver the user has entered the keyword ('r/') and a subreddit name
-chrome.omnibox.onInputEntered.addListener (
-	function(text) {
-		//Append the user-inputted text to the base URL
-		var redditString = 'https://www.reddit.com/r/' + encodeURIComponent(text);
+chrome.omnibox.setDefaultSuggestion({
+  description: 'rSlash: Jump to Subreddit'
+});
 
-		chrome.tabs.query({
-			'currentWindow': true,
-			'active': true
-		}, function(tab) {
-			//Navigate to the URL generated above
-			chrome.tabs.update(tab[0].id, {'url' : redditString});
-		});
-	}
+//Listener for whenver the user has entered the keyword ('r') and a subreddit name
+chrome.omnibox.onInputEntered.addListener (
+  function(text) {
+    //Append the user-inputted text to the base URL
+    var redditString = 'https://www.reddit.com/r/' + text;
+
+    chrome.tabs.query({
+      'currentWindow': true,
+      'active': true
+    }, function(tab) {
+      //Navigate to the URL generated above
+      chrome.tabs.update(tab[0].id, {'url' : redditString});
+    });
+  }
 );
