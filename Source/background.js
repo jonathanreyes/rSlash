0c46397977c10b48f69c639e60065149ac8da87f
Jonathan Reyes <jonathan.js.reyes@gmail.com>
2016-12-02 22:59:41 -0500

    implemented saving visited subs and suggesting them

--- Source/background.js
+++ Source/background.js
@@ -1,8 +1,33 @@
+var prevVisitedSubs = [];
+
+//When rSlash starts, get the saved list of subreddits to suggest
+chrome.omnibox.onInputStarted.addListener ( function() {
+    chrome.storage.sync.get('subredditsList', function (items) {
+      console.log(JSON.stringify(items));
+
+      prevVisitedSubs = items['subredditsList'] ? items['subredditsList'] : [];
+
+      console.log(JSON.stringify(prevVisitedSubs));
+    })
+  }
+);
+
 //Listener for whenever the user changes the text in the omnibox.
 //@TODO: implement subreddit suggestions
-chrome.omnibox.onInputChanged.addListener (
-  function(text, suggest) {
-    return;
+chrome.omnibox.onInputChanged.addListener ( function(text, suggest) {
+    
+    if (prevVisitedSubs.length > 0) {
+      var matchingSuggestions = [];
+      for (var i = 0; i < prevVisitedSubs.length; i++) {
+        if (prevVisitedSubs[i].content.indexOf(text) > -1) {
+          matchingSuggestions.push(prevVisitedSubs[i]);
+        }
+      }
+
+      if (matchingSuggestions.length > 0) {
+        suggest(matchingSuggestions);
+      }
+    }
   }
 );
 
@@ -15,13 +40,27 @@ chrome.omnibox.onInputEntered.addListener (
   function(text) {
     //Append the user-inputted text to the base URL
     var redditString = 'https://www.reddit.com/r/' + text;
-
+    
     chrome.tabs.query({
       'currentWindow': true,
       'active': true
     }, function(tab) {
       //Navigate to the URL generated above
       chrome.tabs.update(tab[0].id, {'url' : redditString});
+
+      //if this is the first time the user has used rSlash to go to this subreddit, save it
+      for (var i = 0; i < prevVisitedSubs.length; i++) {
+        if (prevVisitedSubs[i].content.indexOf(text) > -1)
+          return;
+      }
+
+      prevVisitedSubs.push({content: text, description: 'r/' + text});
+      console.log("adding new item to prevVisitedSubs: " + JSON.stringify(prevVisitedSubs));
+      chrome.storage.sync.set({subredditsList: prevVisitedSubs});
     });
   }
 );
+
+// chrome.omnibox.onInputCancelled.addListener(function () {
+//   return;
+// })
